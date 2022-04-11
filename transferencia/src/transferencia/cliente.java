be93870c63bb4ab84dde94e9713b7ffdd7102ffe
aluno <aluno@DSN1251230708>
2022-04-11 10:39:04 -0300

    pix.teste

---
+++ transferencia/src/transferencia/cliente.java
@@ -0,0 +1,10 @@
+package transferencia;
+
+public class cliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ transferencia/src/transferencia/pix.java
@@ -0,0 +1,10 @@
+package transferencia;
+
+public class pix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
