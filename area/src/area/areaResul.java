312edb36c9a0840600093170a98ed363dccaf9e0
aluno <aluno@DSN1251230708>
2022-04-18 11:18:50 -0300

    new1

---
+++ area/src/area/areaResul.java
@@ -0,0 +1,9 @@
+package area;
+
+public class areaResul {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ area/src/area/calculararea.java
@@ -0,0 +1,24 @@
+package area;
+
+public class calculararea {
+	double calcT(double b, double h);
+
+	{
+
+		double T = b * h;
+
+		return T;
+	}
+
+	int calcQ(int B);
+
+	{
+
+		int x = B * 2;
+
+		return x;
+
+	}
+	
+	int calcR(int x )
+}
