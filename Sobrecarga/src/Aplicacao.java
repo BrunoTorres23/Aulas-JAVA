40046377a5da7c8f03f27ec648d75ec6c9c47cfa
aluno <aluno@DSN1251230708>
2022-04-18 10:34:09 -0300

    2.0

--- Sobrecarga/src/Aplicacao.java
+++ Sobrecarga/src/Aplicacao.java
@@ -1,17 +1,30 @@
+import java.util.Scanner;
 
 public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		Scanner scanner = new Scanner (System.in);
+		
+		
 		matematica Matematica = new matematica();
 		
-		int soma = Matematica.somar(3, 4);
+		System.out.println("Somar dois números");
+		System.out.println("Digite o primeiro número: ");
+		int x = scanner.nextInt();
+		
+		System.out.println("Digite o segundo número: ");
+		int y = scanner.nextInt();
+		
+		
+		
+		int soma = Matematica.somar(x, y);
 		System.out.println("o resultado é: " + soma + "!!!");
 		
-		soma = Matematica.somar(3, 4, 5);
+		soma = Matematica.somar(x, y, 5);
 		System.out.println("o resultado é: " + soma + "!!!");
 		
-		double soma2 = Matematica.somar(3.5, 4);
+		double soma2 = Matematica.somar(x, y);
 		System.out.println("o resultado é: " + soma2 + "!!!");
 	}
 
