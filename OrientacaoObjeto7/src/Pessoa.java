e63979ea220c00f68623b5b74d59c72ac0659b3a
aluno <aluno@DSN1251230708>
2022-04-18 09:11:24 -0300

    Alterações

--- OrientacaoObjeto7/src/Pessoa.java
+++ OrientacaoObjeto7/src/Pessoa.java
@@ -3,27 +3,25 @@ public class Pessoa {
 	String nome;
 	int Numfigurinhas;
 
-	void receber(int numfigurinhas) {
-		this.Numfigurinhas += numfigurinhas;
+	void receber(int numfig) {
+		this.Numfigurinhas += numfig;
 	}
 
-	boolean dar(int numfigurinhas, Pessoa pessoa)
-	
+	boolean dar(int qtdfig , Pessoa pessoa)
+
 	{
-		if (this.Numfigurinhas < numfigurinhas) { //o this.Numfigurinhas foi usado pois como eu já estou dentro dessas variaveis
-
+		if (this.Numfigurinhas < qtdfig) { // o this.Numfigurinhas foi usado pois como eu já estou dentro dessas
+													// variaveis
 			System.out.println("A troca não pode ser realizada");
-			
 			return false;
-	}
-		else {
-
-			this.Numfigurinhas -= numfigurinhas;
-			pessoa.receber(Numfigurinhas);
 			
+		} else {
+
+			this.Numfigurinhas -= qtdfig;
+			pessoa.receber(qtdfig);
+
 			return true;
-	}
+		}
 
-
 	}
-	}
+}
--- OrientacaoObjeto7/src/main.java
+++ OrientacaoObjeto7/src/main.java
@@ -2,33 +2,39 @@
 public class main {
 
 	public static void main(String[] args) {
+
+		Pessoa pessoa1 = new Pessoa(); // Instanceando objeto (ou criando) no caso "pessoa1"
+
+		pessoa1.nome = "Bruno";
+		pessoa1.Numfigurinhas = 4;
+
+		pessoa1.receber(0);
+
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.Numfigurinhas);
+
+		Pessoa pessoa2 = new Pessoa();
+
+		pessoa2.nome = "Gomes";
+		pessoa2.Numfigurinhas = 5;
+
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.Numfigurinhas);
+
+		boolean retorno = pessoa1.dar(1, pessoa2);
+
+		if (retorno == true) {
+		}
+		pessoa1.dar(5, pessoa2);
 		
-		Pessoa pessoa1 = new Pessoa(); //Instanceando objeto (ou criando) no caso "pessoa1"
-		
-		pessoa1.nome = "Bruno"; 
-		pessoa1.Numfigurinhas = 1;
-		
-		pessoa1.receber(3);
-		
-		    System.out.println(pessoa1.nome);
-			System.out.println(pessoa1.Numfigurinhas);
-			
-			Pessoa pessoa2 = new Pessoa();
-			
-			pessoa2.nome = "Gomes";
-			pessoa2.Numfigurinhas = 3;
-			
-			System.out.println(pessoa2.nome);
-			System.out.println(pessoa2.Numfigurinhas);
-			
-			pessoa1.dar(2, pessoa2);
-			
-			System.out.println(pessoa1.nome);
-			System.out.println(pessoa1.Numfigurinhas);
-			
-			System.out.println(pessoa2.nome);
-			System.out.println(pessoa2.Numfigurinhas);
-			
+		System.out.println("Atual valor");
+
+		System.out.println(pessoa1.nome);
+		System.out.println(pessoa1.Numfigurinhas);
+
+		System.out.println(pessoa2.nome);
+		System.out.println(pessoa2.Numfigurinhas);
+
 	}
 
 }
