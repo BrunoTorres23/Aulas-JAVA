0de679ba1e29ebf56d22502645bca79ac9b8fe2d
aluno <aluno@DSN1251230708>
2022-05-02 09:12:56 -0300

    Modificador de acesso

---
+++ ModificadorAcesso/src/Cliente.java
@@ -0,0 +1,40 @@
+
+public class Cliente {
+	private String nome;
+	private int cpf;
+	private int numConta;
+	
+	public Cliente()	{
+}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+
+	public int getNumConta() {
+		return numConta;
+	}
+
+	public void setNumConta(int numConta) {
+		this.numConta = numConta;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente: \nnome = " +  nome + ";  \ncpf = " + cpf + ";  \nnumConta = " + numConta + ".";
+	}
+	
+	
+}
---
+++ ModificadorAcesso/src/main.java
@@ -0,0 +1,44 @@
+
+public class main {
+
+	public static void main(String[] args) {
+
+		//variável (nome da classe) + endereço = construtor de objeto
+		ContaBancaria conta1 = new ContaBancaria();
+		
+//		conta1.ativo = true;
+//		conta1.numConta = 1234;
+//		conta1.saldo = 100.50;
+//		
+//		System.out.println(conta1.ativo);
+//		System.out.println(conta1.numConta);
+//		System.out.println(conta1.saldo); 
+		
+//		//informa o valor a receber
+//		conta1.receber(100.8); 
+//		
+//		//valor a dar
+//		conta1.dar(72.5);
+		
+		//mostra na tela o saldo
+		//System.out.println(conta1.saldo());
+		
+		//ContaBancaria conta2 = new ContaBancaria();
+	
+		//System.out.println(conta2.saldo());
+		
+		Cliente cliente = new Cliente();
+		
+		cliente.setNome("Bruno");
+		cliente.setCpf(484716);
+		cliente.setNumConta(482173);
+		
+//		System.out.println(cliente.getNome());
+//		System.out.println(cliente.getCpf());
+//		System.out.println(cliente.getNumConta());
+		
+		System.out.println(cliente);
+	
+}
+
+}
