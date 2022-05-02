d749e77bb9c210e45def0223f34c6ba6e23dd55a
aluno <aluno@DSN1251230708>
2022-05-02 08:01:11 -0300

    Fundamentos

---
+++ fundamentos/src/fundamentos/main.java
@@ -0,0 +1,126 @@
+package fundamentos;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		//int x = 100;
+		//double y = 200;
+		//int a = 0;
+		//double b = 0;
+		//short z = (short) 10000;
+		//System.out.println(z);
+		
+		//a = (int)y;
+		//b = x;
+		//System.out.println(a);
+		//System.out.println(b);
+		
+		//int idadePessoa = 17;
+		
+		//if(idadePessoa < 18) {
+			//System.out.println("Você não pode beber");
+		//}
+		
+		//else if(idadePessoa < 60) {
+			//System.out.println("Bora tomar uma");
+		//}
+		
+		//else {
+			//System.out.println("Você está na terceira idade");
+		//}
+		
+		//System.out.println("Você tem " + idadePessoa + " anos");
+			
+		//valores constantes usa-se o switch case
+		//o break determina um limite para o seu codigo
+		
+//		int num = 8;
+//		switch (num) {
+//		
+//			case 1:
+//				System.out.println("Hoje é Domingo");
+//					break; 
+//			case 2:
+//				System.out.println("Hoje é Segunda-feira");
+//					break;
+//			case 3:
+//				System.out.println("Hoje é Terça-fera");
+//					break;
+//			case 4:
+//				System.out.println("Hoje é Quarta-feira");
+//					break;
+//			case 5:
+//				System.out.println("Hoje é Quinta-feira");
+//					break;
+//			case 6:
+//				System.out.println("Hoje é Sexta-feira");
+//					break;
+//			case 7: 
+//				System.out.println("Hoje é Sabado");
+//					break;
+//			default:
+//				System.out.println("Esse número não representa um dia da semana");
+//		}
+//		
+//		char caractere = 'a';
+//		
+//		switch (caractere) {
+//		case 'a':
+//			System.out.println("Seu caractere é a");
+//			break;
+//		case 'b':
+//			System.out.println("Seu caractere é b");
+//			break;
+//		default:
+//			System.out.println("valor inválido");
+//			
+//	}
+		
+//			String farol = "verde";
+//				
+//			switch (farol) {
+//			
+//			case "vermelho":
+//				System.out.println("Pare");
+//				break;
+//			case "amarelo":
+//				System.out.println("Atenção");
+//				break;
+//			case "verde":
+//				System.out.println("Prossiga");
+//				break;
+//				
+//			default: 
+//				System.out.println("Valor inválido");
+//				
+//			}
+		
+		//ele obtém a resposta de um teste condicional, ele ocorre meio que preso dentro daquela condição
+//		int x = 0;
+//		while(x < 10) { 
+//		
+//			System.out.println(x);
+//			x = x+1;
+// }
+
+//	int x=0;
+//	while(x<10) {
+//		System.out.println(x);
+//		x=x+1;
+//	}
+//	
+//	x=0;
+//	//ele executa apenas quando a condição é verdadeira, se não ele apenas sai
+//	do {
+//		System.out.println(x);
+//		x=x+1;
+//	} while (x<10);
+	
+		//
+		for(int x=0; x<=10; x++) {
+			System.out.println("9 x " + x + " = " + x*9);
+		}
+	}
+}
+
