dcfc4e767514eac942a09c8acf1aac8aeced8d4d
aluno <aluno@DSN1251017054>
2022-05-05 11:35:15 -0300

    Classe super

---
+++ Polimorfismo/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+//		Animal animal = new Cachorro();
+//		animal.Falar();
+//		
+//		animal = new Gato();
+//		animal.Falar();
+		Gato gato = new Gato();
+		Cachorro cachorro = new Cachorro();
+		falar(gato);
+		falar(cachorro);
+	}
+	
+	public static void falar(Animal a) {
+		a.Falar();
+		
+	}
+	
+
+}
