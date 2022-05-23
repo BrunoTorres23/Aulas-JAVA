50506d34ede0e3eab3d2a1e481e0173d2a05ab38
aluno <aluno@DSN1251230708>
2022-05-23 11:36:07 -0300

    Desafio do cadastro

--- Cadastro/src/Main.java
+++ Cadastro/src/Main.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Scanner entrada = new Scanner(System.in);
+		
+		
+		
+		
 	}
-
+	
+	
 }
--- Cadastro/src/Pessoa.java
+++ Cadastro/src/Pessoa.java
@@ -1,4 +1,39 @@
 
 public class Pessoa {
+	
+	private String nome, endereco;
+	private int idade;
+	
+	private enum Sexo {
+		MASCULINO, FEMININO;
+	}
+	
+	//get and set de mome
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	//get and set de idade 
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	//get and set de endereco
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	
+	
 }
