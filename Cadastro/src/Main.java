573d2592180e671afbbb48d3ce3829881f3375ac
aluno <aluno@DSN1251230708>
2022-05-23 11:47:48 -0300

    Commit do desafio

--- Cadastro/src/Main.java
+++ Cadastro/src/Main.java
@@ -4,10 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		Pessoa pessoa = new Pessoa();
 		
 		Scanner entrada = new Scanner(System.in);
 		
-		
+		System.out.println("Digite seu nome: ");
+	
 		
 		
 	}
