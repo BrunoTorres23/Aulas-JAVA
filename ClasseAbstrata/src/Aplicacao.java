24e5fc294c73c437f2c74d9417d7d34a973ae758
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-04 20:07:10 -0300

    Update Aplicacao.java

--- ClasseAbstrata/src/Aplicacao.java
+++ ClasseAbstrata/src/Aplicacao.java
@@ -1,9 +1,9 @@
-//a aplicacao é a classe que vai executar tudo que foi criado nas sub classes
+//a aplicacao Ã© a classe que vai executar tudo que foi criado nas sub classes
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		Item item = new Cogumelo();
-		item.pegar();
+		Item item = new Cogumelo(); //aqui esta dizendo que "item" vai receber o valor "cogumelo"
+		item.pegar(); //"pegar" vira um metodo
 		
 		item = new DIamante();
 		item.pegar();
