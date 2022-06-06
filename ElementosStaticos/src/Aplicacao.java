373e844b84dde58e8ec110f1066dcbd3397a9376
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:51:22 -0300

    Update Aplicacao.java

--- ElementosStaticos/src/Aplicacao.java
+++ ElementosStaticos/src/Aplicacao.java
@@ -3,21 +3,21 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
-		Matematica matematica = new Matematica();
+		Matematica matematica = new Matematica(); //criacao de objeto
 		
-//		int resultado = matematica.somar(2, 3);
-//		System.out.println(resultado); 
+//		int resultado = matematica.somar(2, 3); //resultado que é um inteiro, vai receber uma conta do tipo soma
+//		System.out.println(resultado); //syso para receber o resultado 
 //		
-//		resultado = matematica.subtrair(10, 4);
+//		resultado = matematica.subtrair(10, 4); //resultado que é um inteiro, vai receber uma conta do tipo subtração
 //		System.out.println(resultado);
 		
-//		int resultado = Matematica.somar(2, 4);
+//		int resultado = Matematica.somar(2, 4); 
 //		System.out.println(resultado);
 //		
 //		resultado  = Matematica.subtrair(11, 11);
 //		System.out.println(resultado);
 		
-//		Contador contador1 = new Contador();
+//		Contador contador1 = new Contador(); //
 //		Contador contador2 = new Contador();
 //		Contador contador3 = new Contador();
 //		
