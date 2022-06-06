d661b4097e9f447b5e5e2977750ae45eea7a4987
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:04:34 -0300

    Update Main.java

--- AreaCalculavel-Interfaces/src/Main.java
+++ AreaCalculavel-Interfaces/src/Main.java
@@ -3,10 +3,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		 Quadrado quadrado = new Quadrado(); //instanciamento de nova variavel que recebe o endereço de Quadrado
-		 quadrado.setLado(4.0);
-		 
-		 quadrado.CalcularArea();
+		 double x = 7; // declaracao de variavel com x igual a 7
+		double y = 6; // declaracao de variavel com y igual a 6
+		
+		AreaCalculavel areaCalculavel = new quadrado(); //areacalculavel recebe quadrado
+		areaCalculavel.calcularArea(x, y); //areacalculavel vezes o metodo 
+		
+		areaCalculavel = new retangulo(); //areacalculavel recebe retangulo
+		areaCalculavel.calcularArea(x, y);  //areacalculavel vezes o metodo 
+		
+		areaCalculavel = new circunferencia(); //areacalculavel recebe circunferencia
+		areaCalculavel.calcularArea(x, y); //areacalculavel vezes o metodo 
+			
 
 	}
 
