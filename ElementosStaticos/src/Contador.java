c2148f30f20a534390266fe6b3b2b1b2d2a20d65
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:54:38 -0300

    Update Contador.java

--- ElementosStaticos/src/Contador.java
+++ ElementosStaticos/src/Contador.java
@@ -1,11 +1,12 @@
 
 public class Contador {
 	
-	private static int valor; 
+	private static int valor; //valor recebe inteiro
 	
 //	public Contador() {
 //		this.valor = 1;
 
+	//getters and setters e constructor 
 	
 	static {
 		valor = 1; 
