e692df683ee6d86f5ee6e69df311d222ade9def5
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 11:03:49 -0300

    Update Ferrari.java

--- Interfaces/src/Ferrari.java
+++ Interfaces/src/Ferrari.java
@@ -1,6 +1,8 @@
 //nas interfaces no lugar de extender, colocamos implements, posso colocar quantas interfaces que prcisar
 public class Ferrari implements Automovel, ItemCaro {
 
+	
+	//sobescrita de metodos que implementa de veiculo
 	@Override
 	public void virarEsquerda() {
 		System.out.println("Ferrari virou a esquerda");
