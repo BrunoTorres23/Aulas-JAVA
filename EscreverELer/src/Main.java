86a864b64be13b5ba6969fd82dceff8c4ca73425
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 07:36:00 -0300

    Update Main.java

--- EscreverELer/src/Main.java
+++ EscreverELer/src/Main.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileReader;
+import java.io.FileReader;		//importações
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
@@ -9,13 +9,13 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		System.out.println("Digite a sua mensagem:");
-		Scanner entrada = new Scanner(System.in);
-		String digitado = (entrada.nextLine());
+		System.out.println("Digite a sua mensagem:");		//syso para aparecer para o usuario uma mensagem 
+		Scanner entrada = new Scanner(System.in);		//scanner entrada para ler e possibilitar o usuario a escrever 
+		String digitado = (entrada.nextLine());			
 		
-		try (BufferedWriter escrever =  new BufferedWriter(new FileWriter("saida.txt"))){
+		try (BufferedWriter escrever =  new BufferedWriter(new FileWriter("saida.txt"))){ //o try com o buffered para caso aconteça algo no codigo o try protege 
 	
-	 			escrever.write(entrada.toString());
+	 			escrever.write(entrada.toString());		//vai ler uma tostring 
 		}
 
 		try(BufferedReader reader = new BufferedReader(new FileReader("saida.txt"))){
@@ -23,7 +23,7 @@ public class Main {
 			String string = "";
 		}
 		
-		System.out.println(digitado);
+		System.out.println(digitado);	//aqui vai ler tudo que foi digitado
 		
 		
 	
