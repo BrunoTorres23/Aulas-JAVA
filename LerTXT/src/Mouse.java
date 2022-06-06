6487d3aaf4e730a84b065cfe5f7a4aa2fc424f7d
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 11:12:39 -0300

    Update Mouse.java

--- LerTXT/src/Mouse.java
+++ LerTXT/src/Mouse.java
@@ -1,16 +1,20 @@
 
 public class Mouse {
 	
+	//atributos de mouse
 	private String funciona;
 	private int qtdbotoes;
 	private String tamanho;
+	
+	//constructor
 	public Mouse(boolean funciona, int qtdbotoes, String tamanho) {
-		
+	
 		super();
 		this.funciona = funciona;
 		this.qtdbotoes = qtdbotoes;
 		this.tamanho = tamanho;
 		
+	
 	}
 	@Override
 	public String toString() {
@@ -19,8 +23,9 @@ public class Mouse {
 	
 	public Mouse(String texto) {
 		String[] arrayAuxiliar1 = texto.split(",");
-		String[] arrayAuxiliar2 = arrayAuxiliar1 [0].split("=");
-		this.funciona = arrayAuxiliar2[1].trim();
+		String[] arrayAuxiliar2 = arrayAuxiliar1 [0].split("=");	//o array para colocar as informaçoes lado a lado
+		this.funciona = arrayAuxiliar2[1].trim();			//o split para quebrar aquilo que não precisa ser mostrado
+										//o trim corta os espaços sobrados
 		
 		String[] arrayAuxiliar1 = arrayAuxiliar1[1].split("=");
 		this.qtdbotoes = arrayAuxiliar2[1].trim();
@@ -29,7 +34,7 @@ public class Mouse {
 		this.tamanho = arrayAuxiliar2[1].trim();
 	}
 	
-	
+	//getters and setters
 	public boolean isFunciona() {
 		return funciona;
 	}
