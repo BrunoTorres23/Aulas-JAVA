f3af3a0797522a2e15c883e258d9193080f4e5df
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 11:18:15 -0300

    Update Main.java

--- LerTXT/src/Main.java
+++ LerTXT/src/Main.java
@@ -1,17 +1,19 @@
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileReader;		//importacoes 
 import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
+	public static void main(String[] args) throws FileNotFoundException, IOException {		//A palavra-chave throws serve para declarar 
+												//que um método pode lançar exceções de um determinado tipo.
 		
-		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))){
-			String line;
+		
+		try(BufferedReader reader = new BufferedReader(new FileReader("entrada.txt"))){	
+			String line;	//usado para ler e vai ser jogado na "string", que recebe texto
 			String string = "";
 			
-			while((line = reader.readLine())!= null) {
+			while((line = reader.readLine())!= null) {	//enquanto o leitor não se deparar com o null, ele vai continuar aparecendo texto
 				string += line + "\n";
 			}
 				System.out.println(string);
