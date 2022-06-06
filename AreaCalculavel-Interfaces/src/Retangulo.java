4e8ce4b93fce43c47b763a71da6c2dedf25f09dc
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:06:55 -0300

    Update Retangulo.java

--- AreaCalculavel-Interfaces/src/Retangulo.java
+++ AreaCalculavel-Interfaces/src/Retangulo.java
@@ -1,4 +1,9 @@
 
-public class Retangulo {
+public class retangulo implements AreaCalculavel{
+
+	@Override
+	public void calcularArea(double x, double y) { //formula do retangulo
+		System.out.println("A area do seu retangulo é: " + x * y);
+	}
 
 }
