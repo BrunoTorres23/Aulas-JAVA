1836b8e9a9cc0ad8f0a514725f59be0723ec05b3
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 10:56:32 -0300

    Update Carro.java

--- Heranca/src/Carro.java
+++ Heranca/src/Carro.java
@@ -1,19 +1,19 @@
 
-public class Carro extends Veiculo {
+public class Carro extends Veiculo { //carro extende de veiculo, veiculo é a classe mae
 	
 	private boolean quatroPortas;
 
 	@Override
 	public void imprimir() {
 		super.imprimir();
-		System.out.println("Tem 4 portas; " + quatroPortas);
+		System.out.println("Tem 4 portas; " + quatroPortas);	//sobescrita de metodo, som proprio de carro
 	}
 
 	public boolean isQuatroPortas() {
 		return quatroPortas;
 	}
 
-	public void setQuatroPortas(boolean quatroPortas) {
+	public void setQuatroPortas(boolean quatroPortas) {	//carro tem quatro portas, para true ou false usa-se o boolean 
 		this.quatroPortas = quatroPortas;
 	}
 	
