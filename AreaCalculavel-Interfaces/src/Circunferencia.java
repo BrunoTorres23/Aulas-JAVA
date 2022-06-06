fdef05c343629c9b4c8c172fa5bca08e3cbd22af
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:00:51 -0300

    Update Circunferencia.java

--- AreaCalculavel-Interfaces/src/Circunferencia.java
+++ AreaCalculavel-Interfaces/src/Circunferencia.java
@@ -2,8 +2,8 @@
 public class circunferencia implements AreaCalculavel{
 
 	@Override
-	public void calcularArea(double x, double y) { // formula da circunferencia, que vai sobescrever o metodo calcular area
-		System.out.println("A area da sua circunferencia é: " + 2 * Math.PI * x);		//math metodo de matematica para calculos, o pi por conta circunferencia 
+	public void calcularArea(double x, double y) { // metodo sobescrito
+		System.out.println("A area da sua circunferencia é: " + 2 * Math.PI * x); //math metodo de matematica para calculos, o pi por conta circunferencia 
     
   }
 	}
