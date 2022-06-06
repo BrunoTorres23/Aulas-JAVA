f6c9210f0eb27fe7c882cb58712b2bc4f2e8f1df
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 08:52:53 -0300

    Update Constante.java

--- ElementosStaticos/src/Constante.java
+++ ElementosStaticos/src/Constante.java
@@ -1,7 +1,7 @@
 
 public class Constante {
 	
-	public static final double MEDIA_DA_PROVA;
+	public static final double MEDIA_DA_PROVA;	//uma constante onde media da prova vira uma constante que a nota minima é 7, e statico pois o valor nao muda
 	
 	static {
 			MEDIA_DA_PROVA = 7.0 ;
