2cfd33608dc52cc7d135a4d84eb79bb3a24d6d95
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 09:09:35 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -12,15 +12,15 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		double pi = Main.PI;
+		double pi = Main.PI; //constante que nao for enum tem que ser guardada em algum lugar
 		
 		DiaDaSemana hoje = DiaDaSemana.SEGUNDA; //ele só aceita os enums que estao no dia da semana 
-		int num = DiaDaSemana.SEGUNDA.getNum();
+		int num = DiaDaSemana.SEGUNDA.getNum(); // pega o dia da semana e o numero do indice que esta ao lado
 		
 		System.out.println(num);
 		
 		String s = "TERCA"; //s esta recebendo terca nesse caso
-		hoje = DiaDaSemana.valueOf(s);
+		hoje = DiaDaSemana.valueOf(s); //Procura a variavel string que está na variavel s e coloca na variavel hoje.
 		
 		System.out.println(hoje.getNum()); //get num sao os dias da semana, 1,2,3 etc... estao atribuidos conforme os dias da semana 
 		
