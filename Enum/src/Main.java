8dc6a656010982f1d85d700d49ba0ea76454e3dd
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-06 09:04:57 -0300

    Update Main.java

--- Enum/src/Main.java
+++ Enum/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 	
 	public static final double PI = 3.14;
 	
+	//o enum cria conjuntos de constantes 
 	private enum Sexo{
 		MASCULINO, FEMININO;
 		
@@ -13,17 +14,17 @@ public class Main {
 		
 		double pi = Main.PI;
 		
-		DiaDaSemana hoje = DiaDaSemana.SEGUNDA; //ele só aceita os enums que estao no dia da semana 
+		DiaDaSemana hoje = DiaDaSemana.SEGUNDA; //ele sÃ³ aceita os enums que estao no dia da semana 
 		int num = DiaDaSemana.SEGUNDA.getNum();
 		
 		System.out.println(num);
 		
-		String s = "TERCA";
+		String s = "TERCA"; //s esta recebendo terca nesse caso
 		hoje = DiaDaSemana.valueOf(s);
 		
-		System.out.println(hoje.getNum());
+		System.out.println(hoje.getNum()); //get num sao os dias da semana, 1,2,3 etc... estao atribuidos conforme os dias da semana 
 		
-		Sexo sexo = Sexo.FEMININO;
+		Sexo sexo = Sexo.FEMININO; //sexo recebe tanto feminino e masculino
 		sexo = Sexo.MASCULINO;
 		
 		
