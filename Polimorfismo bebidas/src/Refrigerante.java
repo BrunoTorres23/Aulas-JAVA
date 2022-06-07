6319e96814a723278707d782bcef2617aec7696a
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 18:30:53 -0300

    Update Refrigerante.java

--- Polimorfismo bebidas/src/Refrigerante.java
+++ Polimorfismo bebidas/src/Refrigerante.java
@@ -2,16 +2,16 @@
 public class Refrigerante extends Bebida {
 
 	public Refrigerante() {
-		
-		super("Refrigerante", false); //refrigerante como nome j�, e o falso para retornar com o boolean
+		//refri recebe false pois não é bebida que será servida quente 
+		super("Refrigerante", false); //refrigerante como nome já, e o falso para retornar com o boolean
 		
 	}
 
 	@Override
 	public void preparar() {
 		
-		super.preparar(); //chamou o super pois � algo que sempre vai ser executado 
-		System.out.println("Colocando gelo no copo");
+		super.preparar(); //chamou o super pois é algo que sempre vai ser executado 
+		System.out.println("Colocando gelo no copo"); 
 		System.out.println("Colocando o refrigerante");
 		
 	}
