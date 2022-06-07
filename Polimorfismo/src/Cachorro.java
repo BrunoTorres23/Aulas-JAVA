b43f35444cfa7c8dc988fba4bf664e9eef58255e
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 18:32:59 -0300

    Update Cachorro.java

--- Polimorfismo/src/Cachorro.java
+++ Polimorfismo/src/Cachorro.java
@@ -1,6 +1,7 @@
 
-public class Cachorro extends Animal {
+public class Cachorro extends Animal { //cachorro extende animal, pois animal é a classe mãe
 
+	//overrride de sobescrita de metodos, sons caracteristicos de cachorro
 	@Override
 	public void Falar() {
 		System.out.println("AUAU");
