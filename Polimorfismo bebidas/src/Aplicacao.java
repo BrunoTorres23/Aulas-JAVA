4ad36f3f0777251a24567471598d451a3b1fa790
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 18:19:38 -0300

    Update Aplicacao.java

--- Polimorfismo bebidas/src/Aplicacao.java
+++ Polimorfismo bebidas/src/Aplicacao.java
@@ -3,11 +3,14 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		//instanciamento de objeto
 		Preparador preparador = new Preparador();
 		
+		//instanciamento de objeto, a partir desse ponto a variavel criada podera ser usada no metodo de prepararbebida
 		LeiteQuente leiteQuente = new LeiteQuente();
 		preparador.prepararBebida(leiteQuente);
 		
+		//refrigerante tbm na mesma aplicação
 		Refrigerante refrigerante = new Refrigerante();
 		preparador.prepararBebida(refrigerante);
 
