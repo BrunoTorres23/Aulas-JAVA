9b0a6f0adc25db0d69645adf74558872a670c0ae
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 18:30:04 -0300

    Update Preparador.java

--- Polimorfismo bebidas/src/Preparador.java
+++ Polimorfismo bebidas/src/Preparador.java
@@ -4,14 +4,17 @@ public class Preparador {
 	public void prepararBebida (Bebida bebida) {
 		 System.out.println("Preparando a bebida " + bebida.getNome());
 		 
+		//ja que a bebida esta recebendo refri e leite quente será usado o get nome para puxar
 		 bebida.preparar(); 
 		 
+		//se a bebida for aquecida, aqui aparecera o leite quente, ja que recebeu um metodo de aquecer e true de boolean 
 		 if (bebida.isAquecer()) {
 			 
 		 		System.out.println("Aquecendo a bebida " + bebida.getNome());
 		 		
 		 }
-		 		
+		 
+		//no final as duas bebidas serão preparadas 
 		 System.out.println("Bebida preparada com sucesso\n");
 			 	
 	}
