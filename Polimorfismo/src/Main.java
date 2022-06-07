fffdb5f8af16871a79489d66e7583d691e3e5b72
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 18:39:00 -0300

    Update Main.java

--- Polimorfismo/src/Main.java
+++ Polimorfismo/src/Main.java
@@ -4,22 +4,23 @@ public class Main {
 	public static void main(String[] args) {
 
 //		Animal animal = new Cachorro();
-//		animal.Falar();
+//		animal.Falar();				//os codigos comentados são maneiras mais longas de executar, o modo abaixo são formas que simplificam
 //		
 //		animal = new Gato();
 //		animal.Falar();
-		Gato gato = new Gato();
-		Cachorro cachorro = new Cachorro();
-		falar(gato);
-		falar(cachorro);
+		Gato gato = new Gato();		//instanciamento de gato
+		Cachorro cachorro = new Cachorro();	//instanciamento de cachorro
+		falar(gato);	//gato receber um metodo de falar 
+		falar(cachorro);	////cachorro receber um metodo de falar 
 	}
 	
+	//ambos os animais vão executar seus sons proprios ja que o falar já estão com o animal que receb os dois animais
 	public static void falar(Animal a) {
 		a.Falar();
 		
 		if (a instanceof Cachorro) {
 			Cachorro c = (Cachorro) a;
-			a.morder();
+			a.morder();	//O operador instanceof testa a presença da função construtora do objeto, que seria o cachorro morder 
 		}
 	}
 	
