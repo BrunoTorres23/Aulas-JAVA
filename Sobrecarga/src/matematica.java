efd3c6f7a2e4805291f1e93222a1c5fdce0bd038
Bruno Torres Almeida <102962639+BrunoTorres23@users.noreply.github.com>
2022-06-07 23:47:06 -0300

    Update matematica.java

--- Sobrecarga/src/matematica.java
+++ Sobrecarga/src/matematica.java
@@ -1,17 +1,22 @@
 
 public class matematica {
+	
+	//somar é um metodo que recebeu parametros de x e y
+	//retorna a soma matematica 
 	int somar(int x, int y) {
 
 		int z = x + y;
 
 		return x + y;
 	}
-
+	
+	//nesse caso foi adicionado mais um valor que seria z, mas a mesma estrutura
 	int somar(int x, int y, int z) {
 		
 		return x + y + z;
 	}
 	
+	//double para numeros que tem virgulas
 	double somar (double x, double y) {
 		
 		return x+y;
